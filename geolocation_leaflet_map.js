5fc7feda9d65f734a92c8fbc36e27baba5aef974
Emiliano Alonzo <emialonzo@gmail.com>
2016-03-07 14:41:47 -0300

    agregando setings para el widget

--- geolocation_leaflet_map.js
+++ geolocation_leaflet_map.js
@@ -18,12 +18,10 @@
       // Work on each map
       $.each(fields, function(instance, data) {
         var instanceSettings = data.settings;
-        // console.info("settings:"+JSON.stringify(instanceSettings));
 
         $.each(data.deltas, function(d, delta) {
 
           id = instance + "-" + d;
-          console.info("delta!:"+JSON.stringify(delta));
 
 
           $("div#" + common_map_id + id  ,context).height(instanceSettings.map_height);
@@ -31,7 +29,6 @@
 
           // Only make this once ;)
           // $("#geolocation-map-" + id).once('geolocationLeaflet', function() {
-          console.log("Atach to: " + common_map_id + id);
           $("div#" + common_map_id + id, context).once().each( function() {
 
             var map_type;
@@ -41,17 +38,14 @@
             var lng = delta.lng;
 
             var latlng = [lat,lng];
-            // var latLng = new google.maps.LatLng(lat, lng);
 
             mapOptions = {
               zoom : parseInt(instanceSettings.map_zoomlevel),
               center : latlng,
               scrollwheel : instanceSettings.map_scrollwheel
             };
-            console.log("lat:" + lat + " lng:" + lng + " latlng:" + latlng);
 
             // Create map
-            // Drupal.geolocationGooglemaps.maps[id] = new google.maps.Map(this, mapOptions);
             var map = L.map(common_map_id + id).setView(latlng, mapOptions.zoom);
             L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
               maxZoom: 19,
@@ -73,42 +67,7 @@
         });
       });
 
-      // var map = L.map("geolocation-map-" + id).setView(latlng, 6);
-      // L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      //   maxZoom: 19,
-      //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      // }).addTo(map);
-
-      // map.dragging.disable();
-      // map.touchZoom.disable();
-      // map.doubleClickZoom.disable();
-      // map.scrollWheelZoom.disable();
-      // map.boxZoom.disable();
-      // var new_event_marker = new L.marker(latlng,{ draggable: false});
-      // new_event_marker.addTo(map);
 
     } //atach
   }; //drupal behaviors
 })(jQuery);
-
-
-// /**
-//  * Provides a farbtastic colorpicker for the fancier widget.
-//  */
-// (function ($) {
-//   Drupal.behaviors.field_example_colorpicker = {
-//     attach: function(context) {
-//       $(".edit-field-example-colorpicker").live("focus", function(event) {
-//         var edit_field = this;
-//         var picker = $(this).closest('div').parent().find(".field-example-colorpicker");
-//
-//         // Hide all color pickers except this one.
-//         $(".field-example-colorpicker").hide();
-//         $(picker).show();
-//         $.farbtastic(picker, function(color) {
-//           edit_field.value = color;
-//         }).setColor(edit_field.value);
-//       });
-//     }
-//   }
-// })(jQuery);
--- geolocation_leaflet_widget.js
+++ geolocation_leaflet_widget.js
@@ -12,18 +12,13 @@
       $.each(Drupal.settings.geolocation.defaults, function(i, mapDefaults) {
         // $("#geolocation-map-" + i).once('defaults', function(){
           // Set map options
-          mapOptions = {
-            zoom: 2,
-          }
-          lat = $('#geolocation-lat-' + i + ' input').attr('value') == false ? mapDefaults.lat : $('#geolocation-lat-' + i + ' input').attr('value');
-          lng = $('#geolocation-lng-' + i + ' input').attr('value') == false ? mapDefaults.lng : $('#geolocation-lng-' + i + ' input').attr('value');
+          mapOptions = Drupal.settings.geolocation.settings;
+          lat = $('#geolocation-lat-' + i + ' input', context).attr('value') == false ? mapDefaults.lat : $('#geolocation-lat-' + i + ' input').attr('value');
+          lng = $('#geolocation-lng-' + i + ' input', context).attr('value') == false ? mapDefaults.lng : $('#geolocation-lng-' + i + ' input').attr('value');
           latlng = [lat, lng];
 
-          console.log("mapDefaults.lat:"+ mapDefaults.lat);
-          console.log("lat:"+ lat);
-          console.log("latlng:"+ latlng);
 
-          $("#geolocation-map-" + i).height(200);
+          $("#geolocation-map-" + i, context).height(mapOptions.map_height);
           var map = L.map("geolocation-map-" + i).setView(latlng, mapOptions.zoom);
           L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -42,9 +37,9 @@
                 // console.debug('#geolocation-lat-' + i + '  input');
                 // console.debug('#geolocation-lat-' + i + ' > input');
                 marker.setLatLng(e.latlng);
-                $('#geolocation-lat-' + i + '  input').val(e.latlng.lat);
-                $('#geolocation-lng-' + i + '  input').val(e.latlng.lng);
-                console.debug($('#geolocation-lat-' + i + ' input').val(), e.latlng);
+                $('#geolocation-lat-' + i + '  input', context).val(e.latlng.lat);
+                $('#geolocation-lng-' + i + '  input', context).val(e.latlng.lng);
+                // console.debug($('#geolocation-lat-' + i + ' input').val(), e.latlng);
               // }
             });//map.on.click
 
@@ -53,25 +48,3 @@
     } //atach
   }; //drupal behaviors
 })(jQuery);
-
-
-// /**
-//  * Provides a farbtastic colorpicker for the fancier widget.
-//  */
-// (function ($) {
-//   Drupal.behaviors.field_example_colorpicker = {
-//     attach: function(context) {
-//       $(".edit-field-example-colorpicker").live("focus", function(event) {
-//         var edit_field = this;
-//         var picker = $(this).closest('div').parent().find(".field-example-colorpicker");
-//
-//         // Hide all color pickers except this one.
-//         $(".field-example-colorpicker").hide();
-//         $(picker).show();
-//         $.farbtastic(picker, function(color) {
-//           edit_field.value = color;
-//         }).setColor(edit_field.value);
-//       });
-//     }
-//   }
-// })(jQuery);
